1a5db6fade42c0f1e4deb4700dead272398b23d4
Douglas <douglasobara97@gmail.com>
2022-12-02 06:56:51 +0300

    Initialize Project Using Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-info text-center">Nexis Test</h1>
+    </>
+  );
+}
+
+export default App;
